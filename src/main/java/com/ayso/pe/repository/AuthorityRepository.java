fafa74b02b121296074c2a779cab97252f6ec35d
mijael888 <mijael888@gmail.com>
2019-07-12 11:45:21 -0500

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/ayso/pe/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ayso.pe.repository;
+
+import com.ayso.pe.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/ayso/pe/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ayso.pe.service.dto;
