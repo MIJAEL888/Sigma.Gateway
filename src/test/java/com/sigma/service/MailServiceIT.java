3e7e6847e1654833569aeecb355d5c2f53004a77
mijael888 <mijael888@gmail.com>
2019-07-12 13:35:33 -0500

    Initial application generated by JHipster-6.1.2

--- src/test/java/com/sigma/service/MailServiceIT.java
+++ src/test/java/com/sigma/service/MailServiceIT.java
@@ -45,7 +45,9 @@ import static org.mockito.Mockito.*;
 @SpringBootTest(classes = GatewayApp.class)
 public class MailServiceIT {
 
-    private static String[] languages = {
+    private static String languages[] = {
+        "en",
+        "es"
         // jhipster-needle-i18n-language-constant - JHipster will add/remove languages in this array
     };
     private static final Pattern PATTERN_LOCALE_3 = Pattern.compile("([a-z]{2})-([a-zA-Z]{4})-([a-z]{2})");
